03de4c399514cf1abe36a669760a461b30172d0d
Akhul Syaifudin <addin@Akhuls-MacBook-Pro.local>
2022-04-27 21:49:21 +0700

    set active menu base on current page

--- functions.php
+++ functions.php
@@ -105,3 +105,12 @@ function addin_posts_link_attributes(){
     return "class='page-link'";
 }
 add_filter("next_posts_link_attributes", 'addin_posts_link_attributes');
+
+
+add_filter('nav_menu_css_class' , 'special_nav_class' , 10 , 2);
+function special_nav_class($classes, $item){
+    if( in_array('current-menu-item', $classes) ){
+        $classes[] = 'active';
+    }
+    return $classes;
+}
